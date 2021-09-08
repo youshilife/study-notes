6a77948b499b705d1db6b7e7d28525d6b36dc8fc
幽室星辰 <youshilife@qq.com>
2021-09-08 17:53:52 +0800

    添加ArticleMapper类

---
+++ src/main/java/life/youshi/studynotes/mapper/ArticleMapper.java
@@ -0,0 +1,79 @@
+package life.youshi.studynotes.mapper;
+
+import life.youshi.studynotes.entity.Article;
+import life.youshi.studynotes.util.MyBatisUtils;
+
+/**
+ * [数据库映射器] 文章
+ *
+ * 提供文章数据的数据库交互操作方法。
+ */
+public class ArticleMapper {
+    // 配置文件中的命名空间
+    private static final String namespace = ArticleMapper.class.getName();
+
+    /**
+     * 根据ID获取文章
+     *
+     * @param id    文章ID
+     * @return      文章实例
+     */
+    public Article selectArticleById(Integer id) {
+        return (Article) MyBatisUtils.executeQuery(
+            sqlSession ->
+                sqlSession.selectOne(namespace + ".selectById", id)
+        );
+    }
+
+    /**
+     * 插入文章
+     *
+     * @param article   文章实例
+     * @return          是否成功
+     */
+    public boolean insertArticle(Article article) {
+        int rows = (int) MyBatisUtils.executeUpdate(
+            sqlSession ->
+                sqlSession.insert(namespace + ".insert", article)
+        );
+        return rows > 0;
+    }
+
+    /**
+     * 更新文章
+     *
+     * @param article   文章实例
+     * @return          是否成功
+     */
+    public boolean updateArticle(Article article) {
+        int rows = (int) MyBatisUtils.executeUpdate(
+            sqlSession ->
+                sqlSession.update(namespace + ".update", article)
+        );
+        return rows > 0;
+    }
+
+    /**
+     * 根据ID删除文章
+     *
+     * @param id    文章ID
+     * @return      是否成功
+     */
+    public boolean deleteArticleById(Integer id) {
+        int rows = (int) MyBatisUtils.executeUpdate(
+            sqlSession ->
+                sqlSession.delete(namespace + ".delete", id)
+        );
+        return rows > 0;
+    }
+
+    /**
+     * 删除文章
+     *
+     * @param article   文章实例
+     * @return          是否成功
+     */
+    public boolean deleteArticle(Article article) {
+        return deleteArticleById(article.getId());
+    }
+}
