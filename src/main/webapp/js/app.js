52f0f8deb95ee5d39e16ad8da43018a821196154
幽室星辰 <youshilife@qq.com>
2021-09-09 20:56:03 +0800

    编写应用的基本样式

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,1 +1,13 @@
 'use strict';
+
+let sidebar = document.querySelector("#sidebar");
+
+let buttonShowSidebar = document.querySelector("#button-show-sidebar");
+buttonShowSidebar.addEventListener("click", function () {
+    sidebar.classList.add("shown");
+});
+
+let buttonHideSidebar = document.querySelector("#button-hide-sidebar");
+buttonHideSidebar.addEventListener("click", function () {
+    sidebar.classList.remove("shown");
+});
