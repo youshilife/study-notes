f91ad81e8593e23c39cdd900151b95e4708a6458
幽室星辰 <youshilife@qq.com>
2021-09-09 23:48:06 +0800

    添加删除文章的Servlet

--- src/main/resources/数据库.sql
+++ src/main/resources/数据库.sql
@@ -6,12 +6,13 @@ CREATE TABLE articles
 (
     id INT AUTO_INCREMENT COMMENT '文章ID',
     parent_id INT NULL COMMENT '父文章ID',
-    path TEXT NOT NULL COMMENT '访问路径',
+    path VARCHAR(512) NOT NULL COMMENT '访问路径',
     sort_code INT NOT NULL DEFAULT 0 COMMENT '排序值',
     title TEXT NOT NULL COMMENT '标题',
     content LONGTEXT NOT NULL COMMENT '内容',
     created_at DATETIME NOT NULL DEFAULT NOW() COMMENT '创建时间',
     updated_at DATETIME NOT NULL DEFAULT NOW() COMMENT '更新时间',
     PRIMARY KEY (id),
-    FOREIGN KEY (parent_id) REFERENCES articles(id)
+    UNIQUE KEY (path),
+    FOREIGN KEY (parent_id) REFERENCES articles(id) ON DELETE CASCADE
 ) COMMENT '文章表';
