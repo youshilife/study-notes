af043869a981f2b21a56dc54e78a6b165ff2a7b1
幽室星辰 <youshilife@qq.com>
2021-09-10 10:08:53 +0800

    重构GetServlet，支持根据id和path获取

--- src/main/java/life/youshi/studynotes/servlet/article/GetServlet.java
+++ src/main/java/life/youshi/studynotes/servlet/article/GetServlet.java
@@ -1,50 +1,48 @@
 package life.youshi.studynotes.servlet.article;
 
 import life.youshi.studynotes.entity.Article;
-import life.youshi.studynotes.service.ArticleService;
 import life.youshi.studynotes.util.JsonResponseUtils;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.net.URLDecoder;
 import java.util.List;
 
 /**
  * [Servlet] 获取文章
  */
-@WebServlet("/api/article")
-public class GetServlet extends HttpServlet {
-    private final ArticleService articleService = new ArticleService();
-
+@WebServlet("/api/article/get")
+public class GetServlet extends ArticleServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String path = URLDecoder.decode(request.getParameter("path"), request.getCharacterEncoding());
-        if (!path.equals("/") && path.endsWith("/")) {
-            path = path.substring(0, path.length() - 1);
-        }
-
-        Article article = articleService.getArticleByPath(path);
-        Article parent = articleService.getParentArticle(article);
-        List<Article> children = articleService.getChildArticles(article);
-
-        if (parent != null) {
-            parent.setContent(null);
-        }
-        if (children != null) {
-            for (Article child : children) {
-                child.setContent(null);
+        handle(response, () -> {
+            Article article;
+            if (request.getParameter("id") != null) {
+                article = checkId(request, "id");
+            } else if (request.getParameter("path") != null) {
+                article = checkPath(request, "path");
+            } else {
+                throw new BadRequestException();
             }
-        }
 
-        JsonResponseUtils result = new JsonResponseUtils()
-            .putData("article", article)
-            .putData("parent", parent)
-            .putData("children", children);
+            Article parent = articleService.getParentArticle(article);
+            List<Article> children = articleService.getChildArticles(article);
 
-        response.getWriter().println(result.toJson());
+            if (parent != null) {
+                parent.setContent(null);
+            }
+            if (children != null) {
+                for (Article child : children) {
+                    child.setContent(null);
+                }
+            }
+
+            return new JsonResponseUtils()
+                .putData("article", article)
+                .putData("parent", parent)
+                .putData("children", children);
+        });
     }
 }
